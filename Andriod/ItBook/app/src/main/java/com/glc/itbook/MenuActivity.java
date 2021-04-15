e2fdebfdfa0a6312c46d76e826e11a34b69a0d8c
luxurer <1161787404@qq.com>
2021-04-15 10:56:40 +0800

    1.定位完成

--- Andriod/ItBook/app/src/main/java/com/glc/itbook/MenuActivity.java
+++ Andriod/ItBook/app/src/main/java/com/glc/itbook/MenuActivity.java
@@ -7,14 +7,19 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import com.glc.itbook.fragment.Fragment_dongtai;
@@ -25,6 +30,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MenuActivity extends AppCompatActivity {
+    private final int SDK_PERMISSION_REQUEST = 127;
+    private ListView FunctionList;
+    private String permissionInfo;
+
     private ViewPager vp;
     private LinearLayout btn1;
     private TextView tv1;
@@ -55,6 +64,8 @@ public class MenuActivity extends AppCompatActivity {
         fragments.add(new Fragment_index());
         fragments.add(new Fragment_dongtai());
         fragments.add(new Fragment_wode());
+
+        getPersimmions();
 
 
         SharedPreferences sharedPreferences=getSharedPreferences("data", Context.MODE_PRIVATE);
@@ -173,6 +184,48 @@ public class MenuActivity extends AppCompatActivity {
         @Override
         public int getCount() {
             return fragments.size();
+        }
+    }
+
+    @TargetApi(23)
+    private void getPersimmions() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            ArrayList<String> permissions = new ArrayList<String>();
+            /***
+             * 定位权限为必须权限，用户如果禁止，则每次进入都会申请
+             */
+            // 定位精确位置
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.ACCESS_FINE_LOCATION);
+            }
+            if (checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                permissions.add(Manifest.permission.ACCESS_COARSE_LOCATION);
+            }
+            /*
+             * 读写权限和电话状态权限非必要权限(建议授予)只会申请一次，用户同意或者禁止，只会弹一次
+             */
+            // 读写权限
+            if (addPermission(permissions, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                permissionInfo += "Manifest.permission.WRITE_EXTERNAL_STORAGE Deny \n";
+            }
+            if (permissions.size() > 0) {
+                requestPermissions(permissions.toArray(new String[permissions.size()]), SDK_PERMISSION_REQUEST);
+            }
+        }
+    }
+
+    @TargetApi(23)
+    private boolean addPermission(ArrayList<String> permissionsList, String permission) {
+        // 如果应用没有获得对应权限,则添加到列表中,准备批量申请
+        if (checkSelfPermission(permission) != PackageManager.PERMISSION_GRANTED) {
+            if (shouldShowRequestPermissionRationale(permission)) {
+                return true;
+            } else {
+                permissionsList.add(permission);
+                return false;
+            }
+        } else {
+            return true;
         }
     }
 }
--- src/main/java/com/glc/loginregister/LoginRegisterApplication.java
+++ src/main/java/com/glc/loginregister/LoginRegisterApplication.java
@@ -1,13 +1,22 @@
 package com.glc.loginregister;
 
+import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LoginRegisterApplication {
+public class LoginRegisterApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LoginRegisterApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(LoginRegisterApplication.class, args);
+	}
+
+	// 不重写打包war部署到tomcat接口会报404
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LoginRegisterApplication.class);
+	}
 
 }
